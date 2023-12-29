bfb0b09ea4d8bd79fc42baa9038080708ff01c31
les4975 <les1999@naver.com>
2023-12-29 17:40:26 +0900

    12.29_StatusInput 이동

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,61 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { IStatus, statusState } from "../atoms";
-
-const StatusInput = ({ id, label, value, max }: IStatus) => {
-  const [status, setStatus] = useRecoilState(statusState);
-  const deleteStatus = useCallback(
-    (event: React.MouseEvent<HTMLButtonElement>) => {
-      const button = event.currentTarget;
-      setStatus((prev) => prev.filter((item) => item && item.id !== id));
-    },
-    [setStatus, id]
-  );
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-
-    setStatus((prev) =>
-      prev.map((item) => {
-        if (item && item.id === id) {
-          if (name === "label") return { ...item, label: value };
-          else if (name === "value") return { ...item, value: parseInt(value) };
-          else if (name === "max") {
-            const maxValue = value === "" ? 0 : parseInt(value);
-            return { ...item, max: maxValue };
-          }
-        }
-        return item;
-      })
-    );
-    console.log(status);
-  };
-
-  return (
-    <li key={id}>
-      <button onClick={deleteStatus}>-</button>
-      <input
-        type="text"
-        name="label"
-        placeholder="라벨"
-        onBlur={handleChange}
-      />
-      <input
-        type="number"
-        name="value"
-        inputMode="numeric"
-        placeholder="현재치"
-        onBlur={handleChange}
-      />
-      <input
-        type="number"
-        name="max"
-        inputMode="numeric"
-        placeholder="최대치"
-        onBlur={handleChange}
-      />
-    </li>
-  );
-};
+import StatusInput from "./inputs/StatusInput";
 
 function StatusList() {
   const [status, setStatus] = useRecoilState(statusState);
---
+++ src/components/inputs/StatusInput.tsx
@@ -0,0 +1,59 @@
+import { useRecoilState } from "recoil";
+import { IStatus, statusState } from "../../atoms";
+import { useCallback } from "react";
+
+function StatusInput({ id, label, value, max }: IStatus) {
+  const [status, setStatus] = useRecoilState(statusState);
+  const deleteStatus = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      const button = event.currentTarget;
+      setStatus((prev) => prev.filter((item) => item && item.id !== id));
+    },
+    [setStatus, id]
+  );
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setStatus((prev) =>
+      prev.map((item) => {
+        if (item && item.id === id) {
+          if (name === "label") return { ...item, label: value };
+          else if (name === "value") return { ...item, value: parseInt(value) };
+          else if (name === "max") {
+            const maxValue = value === "" ? 0 : parseInt(value);
+            return { ...item, max: maxValue };
+          }
+        }
+        return item;
+      })
+    );
+    console.log(status);
+  };
+
+  return (
+    <li key={id}>
+      <button onClick={deleteStatus}>-</button>
+      <input
+        type="text"
+        name="label"
+        placeholder="라벨"
+        onBlur={handleChange}
+      />
+      <input
+        type="number"
+        name="value"
+        inputMode="numeric"
+        placeholder="현재치"
+        onBlur={handleChange}
+      />
+      <input
+        type="number"
+        name="max"
+        inputMode="numeric"
+        placeholder="최대치"
+        onBlur={handleChange}
+      />
+    </li>
+  );
+}
+
+export default StatusInput;
